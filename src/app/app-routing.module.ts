7490bf9c3ac5c9bec459e7d548b7e658a037ce43
maha <grindi.maha@gmail.com>
2022-12-19 11:41:51 +0100

    page profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -150,6 +150,10 @@ const routes: Routes = [
     path: 'notification',
     loadChildren: () => import('./pages/notification/notification.module').then( m => m.NotificationPageModule)
   },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+  },
 
   // {
   //   path: 'createevent',
--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -11,5 +11,34 @@ export class ProfilePage implements OnInit {
 
   ngOnInit() {
   }
+  data = {
+                "ratingNumber":"0",
+                "listCategory":[{"_id":"6391f6301f0fc711426b4432",   "name":"Camping",    "icon":"camping.svg", "__v":{"$numberInt":"0"},    "entry":false},
+                                {"_id":"6391f64f1f0fc711426b4436","name":"Fishing","icon":"fishing.svg",   "entry":false}],
+                "workArea":[{"id":{"$numberInt":"1"},"nom":"Tunis"},{"id":{"$numberInt":"4"},"nom":"Monastir"}],
+
+                "hourPrice": 21 ,
+                "dayPrice": 100,
+                "galerie":["image1.jpg","image2.jpg","image3.jpg"],
+
+
+                "reservationType":[{"0":"2"},{"1":"7"}],
+
+
+
+                "ListOfbestplace":["Marina Sousse","place2","place3","place4"],
+
+
+
+                "verifiedStatus":["image_cin.jpg","b3.pdf"],
+
+
+                "profilePicture":"profile1.jpg",
+
+                 }
+
+      user={ 
+        "username":"dalida","email":"dalida@yahoo.com","phone":"35269845",
+        "guide":true}
 
 }
